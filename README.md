69425a5f994c9295deddfc53538c99dfb1898c4f
Jibran SiddiQui <jibranahmedsiddiqi@gmail.com>
2020-07-30 08:13:07 +0100

    uploading photo to firebase storage

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-# flutter_mvp
+# flutter MVP
 
-A new Flutter project.
+A new Flutter project using MVP Pattern.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+1 - MVP Deisgn pattern
+2 - Networking
+        - Modular Approach
+        - Independent of libraries from business logic
+3 - Interface Implementation
+4 - Firebase Integeration
+5 - Camera & Gallery usage
+6 - Uploading profile image to firebase stogare
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+# Upcoming
+1  - Payment page refine for MVP
--- lib/payment/views/payment_detail.dart
+++ lib/payment/views/payment_detail.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -141,8 +142,12 @@ class _PaymentDetailState extends State<PaymentDetail> implements PaymentView {
                                         backgroundImage: (_payment.imageUrl !=
                                                     "" ||
                                                 _payment.imageUrl != " ")
-                                            ? NetworkImage(_payment.imageUrl)
-                                            : Container(),
+                                            ? NetworkImage("https://firebasestorage.googleapis.com/v0/b/flutter-mvp-179a5.appspot.com/o/profile?alt=media&token=90a70916-1556-4ef5-b89e-a1cd6c387a64")
+                                            : (_uploadedFileURL != null
+                                                ? Image.network(
+                                                    _uploadedFileURL,
+                                                  )
+                                                : Container()),
                                       ),
                                     ),
                                     Positioned(
@@ -166,7 +171,7 @@ class _PaymentDetailState extends State<PaymentDetail> implements PaymentView {
                                                   child: Text("Camera"),
                                                   isDefaultAction: true,
                                                   onPressed: () {
-                                                   chooseCamera();
+                                                    chooseCamera();
                                                   },
                                                 ),
                                                 CupertinoActionSheetAction(
@@ -340,6 +345,7 @@ class _PaymentDetailState extends State<PaymentDetail> implements PaymentView {
       setState(() {
         _image = image;
       });
+      uploadFile();
     });
   }
 
@@ -348,6 +354,7 @@ class _PaymentDetailState extends State<PaymentDetail> implements PaymentView {
       setState(() {
         _image = image;
       });
+      uploadFile();
     });
   }
 
@@ -361,74 +368,16 @@ class _PaymentDetailState extends State<PaymentDetail> implements PaymentView {
       setState(() {
         _uploadedFileURL = fileURL;
         print(_uploadedFileURL);
+        Firestore.instance
+      .collection('payments')
+        .document('JH0WEOMxuzmbT15BYZMq')
+          .updateData({
+            'picture': _uploadedFileURL,
+          });
+      });
+      setState(() {
+       _uploadedFileURL = fileURL;
       });
     });
   }
 }
-
-/*
-File _image;    
-
-String _uploadedFileURL; 
-Center(    
-       child: Column(    
-         children: <Widget>[    
-           Text('Selected Image'),    
-           _image != null    
-               ? Image.asset(    
-                   _image.path,    
-                   height: 150,    
-                 )    
-               : Container(height: 150),    
-           _image == null    
-               ? RaisedButton(    
-                   child: Text('Choose File'),    
-                   onPressed: chooseFile,    
-                   color: Colors.cyan,    
-                 )    
-               : Container(),    
-           _image != null    
-               ? RaisedButton(    
-                   child: Text('Upload File'),    
-                   onPressed: uploadFile,    
-                   color: Colors.cyan,    
-                 )    
-               : Container(),    
-           _image != null    
-               ? RaisedButton(    
-                   child: Text('Clear Selection'),    
-                   //onPressed: clearSelection,    
-                 )    
-               : Container(),    
-           Text('Uploaded Image'),    
-           _uploadedFileURL != null    
-               ? Image.network(    
-                   _uploadedFileURL,    
-                   height: 150,    
-                 )    
-               : Container(),    
-         ],    
-       ),    
-     ), 
-
-  Future chooseFile() async {    
-   await ImagePicker.pickImage(source: ImageSource.gallery).then((image) {    
-     setState(() {    
-       _image = image;    
-     });    
-   });    
- }  
- Future uploadFile() async {    
-   StorageReference storageReference = FirebaseStorage.instance    
-       .ref()    
-       .child('chats/${Path.basename(_image.path)}}');    
-   StorageUploadTask uploadTask = storageReference.putFile(_image);    
-   await uploadTask.onComplete;    
-   print('File Uploaded');    
-   storageReference.getDownloadURL().then((fileURL) {    
-     setState(() {    
-       _uploadedFileURL = fileURL; 
-       print(_uploadedFileURL)   ;
-     });    
-   });    
- } */
